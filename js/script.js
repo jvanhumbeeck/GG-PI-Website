092b0e12067d648b52631f3cfb06cfec557295e6
jensvh <jvanhumbeeck@humaniora.be>
2017-11-02 14:59:53 +0100

    Adding the slide show.
    Auto play, stops when press next.
    Adding some titles and subtitles with some contrasting colours.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+/* initial */
+var i = 0;
+var t = true;
+var id = setTimeout(carousel, 3000, 1);
+
+/* onload event */
+window.addEventListener("load", function(){
+	fixButton();
+	fixImage();
+	
+	document.getElementById("lbutton").addEventListener("click", function() {
+		clearTimeout(id);
+		t = false;
+		
+		carousel(-1);
+	});
+	
+	document.getElementById("rbutton").addEventListener("click", function() {
+		clearTimeout(id);
+		t = false;
+		
+		carousel(1);
+	});
+	
+});
+
 /* navbar collapse script */
 document.getElementById("collapsor").onclick = function() {
 	
@@ -17,9 +43,76 @@ document.getElementById("collapsor").onclick = function() {
 /* resize event for navbar recollapse at pixels */
 window.addEventListener("resize", function() {
 	
+	/* carousel */
+	fixImage();
+	fixButton();
+	
 	if(window.innerWidth > 600) {
 		var navbar = document.getElementById("navbar");
 		navbar.style.height = "60px";
 		navbar.setAttribute("state", "false");
 	}
 });
+
+/* carousel */ /* for max height 100%; && titles */
+function fixImage() {
+	
+	if(document.getElementById("carousel").clientHeight > window.innerHeight) {
+		
+		document.getElementById("carousel").style.height = window.innerHeight + "px";
+		var titles = document.getElementsByClassName("holder");
+		
+		for (var i=0; i<titles.length; i++) {
+			titles[i].style.top = "25%";
+		}
+	}else{
+		
+		document.getElementById("carousel").style.height = null;
+		
+		var titles = document.getElementsByClassName("holder");
+		
+		for (var i=0; i<titles.length; i++) {
+			titles[i].style.top = null;
+		}
+	}
+	
+}
+
+function fixButton() {
+	
+	var a = document.getElementsByClassName("slide")[i].getElementsByTagName("img")[0].clientHeight;
+	
+	document.getElementById("lbutton").style.height = a + "px";
+	document.getElementById("rbutton").style.height = a + "px";
+	
+}
+
+function carousel(x) {
+	
+	
+	
+		var a = document.getElementsByClassName("slide");
+		
+		if((i == 2) && (x > 0)){
+			
+			a[i].style.display = "none";
+			
+			i = 0;
+			
+			a[i].style.display = "inline-block";
+			
+		}else{
+			
+			a[i].style.display = "none";
+			
+			i += x;
+			
+			a[i].style.display = "inline-block";
+			
+		}
+		
+		if(t == true){
+		
+		    id = setTimeout(carousel, 3000, 1);
+	    }
+}
