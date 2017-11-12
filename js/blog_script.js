7a27ab45341aca7b804362562153d9ced5cd73e3
jensvh <jvanhumbeeck@humaniora.be>
2017-11-12 15:40:10 +0100

    Add blog posts, button to show more and less posts

--- js/blog_script.js
+++ js/blog_script.js
@@ -1,8 +1,5 @@
 /* initializer */
-var posts = 0 ;
-
 registerImages();
-showPosts(5);
 
 /* navbar collapse script */
 document.getElementById("collapsor").addEventListener("click", function() {
@@ -111,46 +108,74 @@ function showImg(img) {
 }
 
 /* blog post show a few, not all */
+//shown posts
+var shown_posts = 0 ;
+
+//initializer
+showPosts(5);
+
 function showPosts(amount) {
+	// all the posts, or the max possible shown posts
 	var p = document.getElementsByClassName("post");
+	var max = p.length;
+	var min = 5;
 	
-	if(amount > 0){
-		var q = ((amount + posts) > p.length) ? (p.length + posts) : (amount + posts); 
-		for(var i = posts;i < q; i++) {
-			posts++;
-			p[i].style.display = "block";
+	//amount is the amount to sow or hide;
+	if(amount > 0){//show amount more posts
+		//check if is possible to show amount of posts, and calculate the amount that is possible;
+		var q = ((shown_posts + amount) > max) ? (max - shown_posts) : amount;
+		//q is the amount to show more;
+		
+		//loop trought to all q's;
+		for(var i = 0; i < q; i++){
+			//show post;
+			p[shown_posts + i].style.display = "block";
 		}
-	}else{
-		var q = ((amount + posts) < 5) ? (posts - (amount + posts)) : (amount + posts);
-		for(var i = (posts-1);i > (q - 1); i--) {
-			posts--;
-			p[i].style.display = "none";
+		
+		//update shown_posts;
+		shown_posts += q;
+		
+	}else{//hide amount of posts
+		//check if is possible to hide amount of posts(amount is negative), and calculate the amount that is possible;
+		var q = ((shown_posts + amount) < min) ? (shown_posts - min) : (-amount);
+		//q is amount to hide;
+		
+		//loop trought all q's;
+		for(var i = 0; i < q; i++){
+			//hide post, subtract one, to count in Javascript;
+			p[(shown_posts - 1) - i].style.display = "none";
 		}
+		
+		//update shown_posts;
+		shown_posts -= q;
 	}
 	
-	if(posts == p.length){
-		//at bottom of posts
+	//see if it is possible to show more posts, if not, disable button;
+	if(shown_posts == max) {
 		document.getElementById("older").classList.add("disabled");
-	}else {
+	}else{
+		//else remove disabled
 		document.getElementById("older").classList.remove("disabled");
 	}
-	
-	if(posts > 5) {
-		//at top of posts
-		document.getElementById("newer").classList.remove("disabled");
-	}else {
+	//see if it is posssible to hide more posts(the minimum is five), if not, disable button;
+	if(shown_posts == 5) {
 		document.getElementById("newer").classList.add("disabled");
+	}else{
+		//else remove disabled
+		document.getElementById("newer").classList.remove("disabled");
 	}
 }
 
 document.getElementById("older").addEventListener("click", function() {
 	
+	//when press button, show 5 more posts;
 	showPosts(5);
 	
 });
 
 document.getElementById("newer").addEventListener("click", function() {
 	
+	//when press button, hide 5 posts;
 	showPosts(-5);
 	
 });
