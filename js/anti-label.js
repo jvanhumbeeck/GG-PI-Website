929051a0104c8abff1b2c0feec26b9f259e757f1
jensvh <jvanhumbeeck@humaniora.be>
2017-12-24 12:09:23 +0100

    Remove watermark.

---
+++ js/anti-label.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+	
+	document.getElementById("edit").innerHTML = "Label is removed";
+	
+	var divs = document.getElementsByTagName("div");
+	
+	for(var i = 0; i < divs.length; i++) {
+		
+		if(divs[i].style.zIndex == "9999999") {
+			
+			divs[i].style.visibility = "hidden";
+			
+		}
+		//divs[i].style.z-index = "0";
+		
+	}
+	
+}
